1ff053a7037e907af4a60cfdd34eca29da75f6f3
Pravin  Mishra <diatm.pravin.it.07.27@gmail.com>
2012-07-31 06:05:33 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EveryThingWithGit::Application.config.secret_token = 'a7db7620a9454c7c68de16439e9c332f1b5eb398732c75266f17b076ebc05aedbd34427e4103ad52d99160a225c7851cf305f1df49ec8c186dc5ef8ef5ff3727'
